a9b25d2ec34050ed04654160e7552b91e7e11a82
Jeff Nuyts <NUYJE@sintcordula.be>
2026-04-28 12:55:57 +0200

    Project einde les 28/04/2026

--- Bulky/Models/Category.cs
+++ Bulky/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bulky.Models
 {
@@ -7,8 +8,9 @@ namespace Bulky.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
