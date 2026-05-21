712de897c2df5ca431d8f85723f87fcfb8857bf7
Jeff Nuyts <NUYJE@sintcordula.be>
2026-05-21 10:06:06 +0200

    backup les 21/05/2026

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Bulky.Data;
 using Bulky.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Controllers
@@ -22,13 +23,104 @@ namespace Bulky.Controllers
         {
             return View();
         }
-        
+
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges(); 
-            return RedirectToAction("Index", "Category");
+            //Zelf custom validatie toevoegen aan website 
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order cannot exactly match the name.");
+            }
+
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Name test is not allowed.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                TempData["gelukt"] = "Category added successfully"; 
+
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            //Controle op ID
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //op basis van ID het juiste Category object uit _db ophalen
+            Category? categoryFromDb = _db.Categories.Find(id); //.Find werkt enkel op primary key! 
+            //andere methoden om dit te doen: 
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //met FirstOrDefault ook mogelijk om op andere velden dan de primary key te zoeken
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited!";
+                //Je wilt een actie van de controller met de naam Index terug uitvoeren
+                //hiermee worden alle categories terug geladen en getoond en ga je terug naar deze pagina
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            //Controle op ID
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //op basis van ID het juiste Category object uit _db ophalen
+            Category? categoryFromDb = _db.Categories.Find(id); //.Find werkt enkel op primary key! 
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
         }
     }
 }
--- Bulky/Models/Category.cs
+++ Bulky/Models/Category.cs
@@ -9,8 +9,10 @@ namespace Bulky.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Value must be between 1 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
