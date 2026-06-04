a33c64dd9e2b0f7181151bbea7bf19a197cb25f4
Jeff Nuyts <NUYJE@sintcordula.be>
2026-06-04 09:59:59 +0200

    backup les 20260604 Werken met afbeeldingen klaar

--- Bulky/Controllers/ProductController.cs
+++ Bulky/Controllers/ProductController.cs
@@ -3,12 +3,17 @@ using Bulky.Models;
 using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Hosting; 
 
 namespace BulkyWeb.Controllers
 {
     public class ProductController : Controller
     {
+        //het _db object maakt het mogelijk om Entity Framework te gebruiken
         private readonly ApplicationDbContext _db;
+
+        //Mogelijk maken om met afbeeldingen te werken via volgend object
+        private readonly IWebHostEnvironment _webHostEnvironment; 
 
         //aan de constructor geef je een ApplicationDbContext object mee 
         //we hebben dit geregistreerd in de services (Program.cs)
@@ -16,9 +21,10 @@ namespace BulkyWeb.Controllers
         //die we eerder maakten (incl connections string)
 
         //constructor 
-        public ProductController(ApplicationDbContext db)
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment; 
         }
         public IActionResult Index()
         {
@@ -41,12 +47,25 @@ namespace BulkyWeb.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM obj)
+        public IActionResult Create(ProductVM obj, IFormFile? file)
         {
 
 
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    obj.Product.ImageUrl = @"\images\product\" + fileName;
+                }
 
                 _db.Products.Add(obj.Product);
                 //enkel na volgende lijn ga je effectief wegschrijven naar de database. 
@@ -65,37 +84,78 @@ namespace BulkyWeb.Controllers
         }
         public IActionResult Edit(int? id)
         {
-            //Controle op ID
+            //Lijst van categories samenstellen om dropdownlist te vullen
+            IEnumerable<SelectListItem> categoryList =
+                _db.Categories.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+
+            //Werd er vanuit de view een ID mee gegeven? 
             if (id == null || id == 0)
             {
                 return NotFound();
             }
 
             //op basis van ID het juiste Category object uit _db ophalen
-            Product? productFromDb = _db.Products.Find(id); //.Find werkt enkel op primary key! 
+            Product? productFromDb = _db.Products.Find(id);
 
+            //product gevonden?
             if (productFromDb == null)
             {
                 return NotFound();
             }
-            return View(productFromDb);
+
+            ProductVM productVM = new ProductVM();
+            productVM.Product = productFromDb;
+
+            productVM.CategoryList = categoryList;
+            return View(productVM);
+            
+            
         }
 
         [HttpPost]
-        public IActionResult Edit(Product obj)
+        public IActionResult Edit(ProductVM obj, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                _db.Products.Update(obj);
-                _db.SaveChanges();
-                TempData["success"] = "Product edited!";
-                //Je wilt een actie van de controller met de naam Index terug uitvoeren
-                //hiermee worden alle categories terug geladen en getoond en ga je terug naar deze pagina
-                return RedirectToAction("Index", "Product");
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    if (!string.IsNullOrEmpty(obj.Product.ImageUrl))
+                    {
+                        //oude afbeelding verwijderen, hiervoor hebben we het pad naar afbeelding nodig
+                        //in database staat er in het begin een backslash, die moeten we verwijderen
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    obj.Product.ImageUrl = @"\images\product\" + fileName;
+
+                    _db.Products.Update(obj.Product);
+                    _db.SaveChanges();
+                    TempData["success"] = "Product edited!";
+                    //Je wilt een actie van de controller met de naam Index terug uitvoeren
+                    //hiermee worden alle categories terug geladen en getoond en ga je terug naar deze pagina
+                    return RedirectToAction("Index", "Product");
+                }
             }
-            return View();
-
-
+            return View(obj);
         }
 
         public IActionResult Delete(int? id)
